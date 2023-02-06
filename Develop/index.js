f54093614bb1fc5f3a3b69dc12276c7d8c39331c
Tiffany Jones <eight19realty@gmail.com>
2023-02-06 13:51:41 -0500

    added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,97 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require(".utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateHTML = ({ name, location, github, linkedin }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="p-5 mb-4 header bg-light">
+    <div class="container">
+      <h1 class="display-4">Hi, My name is ${name}</h1>
+      <p class="lead">I am from ${location}.</p>
+      <h3>Example heading <span class="badge bg-secondary">My Contact Information</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${github}</li>
+        <li class="list-group-item">LinkedIn: ${linkedin}</li>
+      </ul>
+    </div>
+  </header>
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'why',
+      message: 'Why did you create this project?',
+    },
+    {
+      type: 'input',
+      name: 'benefits',
+      message: 'What are the benefits of using this application?',
+    },
+    {
+      type: 'input',
+      name: 'screenshot',
+      message: 'Add a screenshot by using '
+    },
+    {
+      type: 'input',
+      name: 'knowledge',
+      message: 'What knowledge was gained from building this project?',
+    },
+    {
+      type: 'input',
+      name: 'features',
+      message: 'Does your project include any key features and is so, what?',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'How can your application be installed by other users?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
